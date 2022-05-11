ae43d29793d7d5433d5a8312434b8dc337f1e08d
johnBWeiss <yoniw7@gmail.com>
2022-05-11 11:04:55 +0300

    pagination works need to outsource to smaller functions

--- src/app/components/movie-gallery/movie-gallery.component.ts
+++ src/app/components/movie-gallery/movie-gallery.component.ts
@@ -6,6 +6,9 @@ import { movieService } from 'src/app/services/movie-items.service';
   styleUrls: ['./movie-gallery.component.css'],
 })
 export class MovieGalleryComponent implements OnInit {
+  itemsOnDisplay: number = 10;
+  pageCounter: any = 1;
+  rawMovieData: any = [];
   movies: any = {
     items: [],
   };
@@ -14,10 +17,94 @@ export class MovieGalleryComponent implements OnInit {
 
   searchApi() {
     this.movies = this.movieData.getPopularMovies().subscribe((data: any) => {
-      console.log(data);
-      this.movies = data;
+      this.rawMovieData = data;
+      this.movies = this.rawMovieData.items.slice(0, this.itemsOnDisplay);
     });
     // this.http.get(this.ROOT_URL + '/example');
     // movieService.getPopularMovies()
+  }
+
+  moviePagination(back: boolean, forth: boolean) {
+    if (forth) {
+      this.pageCounter++;
+      console.log(this.pageCounter);
+
+      this.paginationForth();
+    } else {
+      if (this.pageCounter <= 1) {
+        this.movies = this.rawMovieData.items.slice(0, this.itemsOnDisplay);
+        console.log(
+          this.movies,
+          this.pageCounter,
+          this.itemsOnDisplay + this.itemsOnDisplay
+        );
+      }
+
+      if (this.pageCounter > 1) {
+        this.pageCounter--;
+        console.log(
+          this.movies,
+          this.pageCounter,
+          this.pageCounter * this.itemsOnDisplay + this.itemsOnDisplay
+        );
+
+        // if(this.pageCounter===1)
+        this.paginationBack();
+      }
+    }
+  }
+
+  // pageSlicer(operator){
+  //   if (operator==='+')
+  //   this.movies = this.rawMovieData.items.slice(
+  //     this.pageCounter * this.itemsOnDisplay,
+  //     this.pageCounter * this.itemsOnDisplay + this.itemsOnDisplay
+  //   );
+  // }
+
+  paginationBack() {
+    this.movies = this.rawMovieData.items.slice(
+      this.pageCounter * this.itemsOnDisplay - this.itemsOnDisplay,
+      this.pageCounter * this.itemsOnDisplay
+    );
+    console.log(
+      this.movies,
+      this.pageCounter,
+      this.pageCounter * this.itemsOnDisplay,
+      this.pageCounter * this.itemsOnDisplay + this.itemsOnDisplay
+    );
+  }
+  paginationForth() {
+    if (
+      //test if there are less items left in the array than 10
+      this.pageCounter * this.itemsOnDisplay <
+      this.rawMovieData.items.length
+    ) {
+      this.movies = this.rawMovieData.items.slice(
+        this.pageCounter * this.itemsOnDisplay - this.itemsOnDisplay,
+        this.pageCounter * this.itemsOnDisplay
+      );
+      console.log(this.movies, this.pageCounter);
+    } else {
+      let emptyCheck = this.rawMovieData.items.slice(
+        this.pageCounter * this.itemsOnDisplay - this.itemsOnDisplay,
+        this.rawMovieData.items.length
+      );
+      if (emptyCheck[0].rank) {
+        this.movies = emptyCheck;
+        console.log(
+          this.movies,
+          this.pageCounter,
+          this.itemsOnDisplay + this.itemsOnDisplay
+        );
+
+        this.pageCounter--;
+        console.log(
+          this.movies,
+          this.pageCounter,
+          this.itemsOnDisplay + this.itemsOnDisplay
+        );
+      }
+    }
   }
 }
