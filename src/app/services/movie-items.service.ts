f77fdbb0ad175e5c51bae6c6893281162b927a53
johnBWeiss <yoniw7@gmail.com>
2022-05-13 00:06:46 +0300

    dynamic card

--- src/app/services/movie-items.service.ts
+++ src/app/services/movie-items.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+// import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +11,7 @@ export class movieService {
   constructor(private http: HttpClient) {}
 
   getOneMovie(searchWord: any): Observable<[]> {
-    return this.http.get<[]>(`  http://localhost:5000/${searchWord}`, {
+    return this.http.get<[]>(`http://localhost:5000/${searchWord}`, {
       observe: 'body',
       // responseType: 'json',
       //   headers: new HttpHeaders().set('JsonOdds-API-Key', this.apiKey),
